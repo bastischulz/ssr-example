f1b441e01cb7cff2b31e4b23fb527e26f29a4e9a
sebastianschulz <s.schulz@satzmedia.de>
2019-06-14 17:25:36 +0200

    second example added

--- src/AsyncComponent.js
+++ src/AsyncComponent.js
@@ -5,6 +5,8 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Drawer from "@material-ui/core/Drawer";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(theme => ({
     p: {
@@ -15,6 +17,9 @@ const useStyles = makeStyles(theme => ({
 const AsyncComponent = ({ children }) => {
     const [open, setOpen] = useState(false)
     const { p } = useStyles()
+
+
+    // example 1
 
     return (
         <>
@@ -30,6 +35,26 @@ const AsyncComponent = ({ children }) => {
             <Button onClick={() => { setOpen(true) }}>Open</Button>
         </>
     )
+
+    // example 2
+
+    // return (
+    //     <div>
+    //         <Button aria-controls="simple-menu" aria-haspopup="true" onClick={() => { setOpen(true) }}>
+    //             Open Menu
+    //         </Button>
+    //         <Menu
+    //             id="simple-menu"
+    //             keepMounted
+    //             open={open}
+    //             onClose={() => setOpen(false)}
+    //         >
+    //             {children.map(({ name }) => (
+    //                 <MenuItem onClick={() => { setOpen(false) }}>{name}</MenuItem>
+    //             ))}
+    //         </Menu>
+    //     </div>
+    // )
 }
 
 export default AsyncComponent
